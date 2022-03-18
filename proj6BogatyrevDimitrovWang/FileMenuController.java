1e6ffb11a3f0855d555b58b8c3cfe5eb2a59e187
Caiteoro <65575745+Caiteoro@users.noreply.github.com>
2022-03-18 18:01:32 -0400

    Update FileMenuController.java

--- proj6BogatyrevDimitrovWang/FileMenuController.java
+++ proj6BogatyrevDimitrovWang/FileMenuController.java
@@ -1,18 +1,44 @@
 package proj6BogatyrevDimitrovWang;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Tooltip;
-import javafx.stage.FileChooser;
-
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.stage.FileChooser;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+
+import org.fxmisc.flowless.VirtualizedScrollPane;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.StyleClassedTextArea;
 
 public class FileMenuController {
+
+    private TabPane tabPane;
+    // List of saved tabs and their content
+    private HashMap<Tab, String> savedContents = new HashMap<>();
+    // List of saved tabs and their saving path
+    private HashMap<Tab, String> savedPaths = new HashMap<>();
+    // Keep track of the id for new tabs created
+    private int newTabID = 1;
 
     @FXML
     private void handleOpen(ActionEvent event) {
@@ -57,6 +83,119 @@ public class FileMenuController {
         }
     }
 
+    /**
+     * helper function to get the currently selected tab in tabPane
+     *
+     * @return Tab the selected tab
+     */
+    private Tab getSelectedTab() {
+        return tabPane.getSelectionModel().getSelectedItem();
+    }
+
+
+
+    /**
+     * helper function to get the text box in the selected tab
+     *
+     * @return TextArea the text box in the selected tab
+     */
+    private CodeArea getSelectedTextBox() {
+        Tab currentTab = getSelectedTab();
+        VirtualizedScrollPane scrollPane;
+        scrollPane = (VirtualizedScrollPane) currentTab.getContent();
+        return (CodeArea) scrollPane.getContent();
+    }
+
+    /**
+     * Helper method that checks if the text in the selected tab is saved.
+     *
+     * @return boolean whether the text in the selected tab is saved.
+     */
+    private boolean selectedTabIsSaved() {
+        // if tab name has been changed and current text matches with
+        // the saved text in record
+        if (this.savedContents.containsKey(getSelectedTab()) &&
+                getSelectedTextBox().getText().equals(
+                        this.savedContents.get(getSelectedTab()))) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Helper method that handles unsaved text and closes the tab. Shows a dialog
+     * if the text gets modified since its last save or has never been saved.
+     * Closes the tab if the text has been saved or user confirms.
+     *
+     * @param event   An ActionEvent object that gives information about the event
+     *                and its source.
+     * @param exiting a string used in Dialog modification. represents if the
+     *                program
+     *                is exiting or simply closing a single tab
+     *
+     * @return Optional the Optional object returned by dialog.showAndWait().
+     *         returns null if tab text is already saved.
+     */
+    private Optional<ButtonType> closeSelectedTab(ActionEvent event, String exiting) {
+        // if content is not saved
+        if (!selectedTabIsSaved()) {
+            Dialog dialog = DialogOptions.getUnsavedChangesDialog(
+                    getSelectedTab().getText(), exiting);
+
+            Optional<ButtonType> result = dialog.showAndWait();
+            // call handleSave() if user chooses YES
+            if (result.get() == ButtonType.YES) {
+                this.handleSave(event);
+                // keep the tab if the save is unsuccessful (eg. canceled)
+                if (!this.selectedTabIsSaved()) {
+                    return result;
+                }
+            }
+            // quit the dialog and keep selected tab if user chooses CANCEL
+            else if (result.get() == ButtonType.CANCEL) {
+                return result;
+            }
+        }
+        // remove tab from tabPane if text is saved or user chooses NO
+        this.savedContents.remove(getSelectedTab());
+        this.savedPaths.remove(getSelectedTab());
+        tabPane.getTabs().remove(getSelectedTab());
+        return Optional.empty();
+    }
+
+
+    @FXML
+    private void handleNew(ActionEvent event) {
+        // create a new tab
+        this.newTabID++;
+        Tab newTab = new Tab("Untitled-" + this.newTabID);
+        newTab.setOnCloseRequest(new EventHandler<Event>() {
+            @Override
+            public void handle(Event event) {
+                handleClose(new ActionEvent());
+                event.consume();
+            }
+        });
+
+        // create a code area
+        JavaCodeArea javaCodeArea = new JavaCodeArea();
+        CodeArea codeArea = javaCodeArea.getCodeArea();
+        newTab.setContent(new VirtualizedScrollPane<>(codeArea));
+        // add new tab to the tabPane
+        tabPane.getTabs().add(newTab);
+        // make the newly created tab the topmost
+        tabPane.getSelectionModel().selectLast();
+        newTab.setOnCloseRequest(new EventHandler<Event>() {
+            @Override
+            public void handle(Event event) {
+                tabPane.getSelectionModel().select(newTab);
+                handleClose(new ActionEvent());
+                event.consume();
+            }
+        });
+    }
+
 
     /**
      * Handler method for menu bar item Save. Behaves like Save as... if the text
@@ -89,6 +228,56 @@ public class FileMenuController {
         }
     }
 
+
+    /**
+     * Helper method to display error message to user when an exception is thrown
+     *
+     * @type this type is default since main need to use it to print possible exception message
+     */
+    void exceptionAlert(Exception ex){
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        ex.printStackTrace(pw);
+        pw.close();
+        String exceptionText = sw.toString();
+
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Exception Alert");
+        alert.setHeaderText(exceptionText.split("\\s+")[0]);
+        alert.setContentText("An exception has been thrown.");
+        Label label = new Label("The exception stacktrace is:");
+
+        TextArea textArea = new TextArea(exceptionText);
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(label, 0, 0);
+        expContent.add(textArea, 0, 1);
+
+        alert.getDialogPane().setExpandableContent(expContent);
+
+        alert.showAndWait();
+    }
+
+    /**
+     * Handler method for menu bar item Close. It creates a dialog if
+     * the selected tab is unsaved and closes the tab.
+     *
+     * @param event An ActionEvent object that gives information about the event
+     *              and its source.
+     */
+    @FXML
+    public void handleClose(ActionEvent event) {
+        // call helper method
+        this.closeSelectedTab(event, "close");
+    }
 
     /**
      * Handler method for menu bar item Save as.... a dialog appears
